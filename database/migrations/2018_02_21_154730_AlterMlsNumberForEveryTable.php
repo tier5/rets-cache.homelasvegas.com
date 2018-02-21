8e3cb91aaa48a9a36c2f2a0e8213c39714bfbf8b
Tier5 LLC <github@tier5.us>
2018-02-21 21:52:44 +0530

    Rets api V6.4.6 stable released to dev

--- database/migrations/2018_02_21_154730_AlterMlsNumberForEveryTable.php
+++ database/migrations/2018_02_21_154730_AlterMlsNumberForEveryTable.php
@@ -14,33 +14,33 @@ class AlterMlsNumberForEveryTable extends Migration
     public function up()
     {
       Schema::table('property_details', function (Blueprint $table) {
-          $table->char('MLSNumber',20)->index()->unique()->change();
-          $table->timestamp('updated_at')->index()->change();
+          $table->bigInteger('MLSNumber')->index()->unique()->change();
+          $table->dateTime('updated_at')->index()->change();
       });
       Schema::table('property_additionals', function (Blueprint $table) {
-          $table->char('MLSNumber',20)->index()->unique()->change();
-          $table->char('PublicAddressYN',20)->index()->change();
+          $table->bigInteger('MLSNumber')->index()->unique()->change();
+          $table->boolean('PublicAddressYN')->index()->change();
       });
       Schema::table('property_external_features', function (Blueprint $table) {
-          $table->char('MLSNumber',20)->index()->unique()->change();
+          $table->bigInteger('MLSNumber')->index()->unique()->change();
       });
       Schema::table('property_features', function (Blueprint $table) {
-          $table->char('MLSNumber',20)->index()->unique()->change();
+          $table->bigInteger('MLSNumber')->index()->unique()->change();
       });
       Schema::table('property_financial_details', function (Blueprint $table) {
-          $table->char('MLSNumber',20)->index()->unique()->change();
+          $table->bigInteger('MLSNumber')->index()->unique()->change();
       });
       Schema::table('property_interior_features', function (Blueprint $table) {
-          $table->char('MLSNumber',20)->index()->unique()->change();
+          $table->bigInteger('MLSNumber')->index()->unique()->change();
       });
       Schema::table('property_lat_longs', function (Blueprint $table) {
-          $table->char('MLSNumber',20)->index()->unique()->change();
+          $table->bigInteger('MLSNumber')->index()->unique()->change();
       });
       Schema::table('property_locations', function (Blueprint $table) {
-          $table->char('MLSNumber',20)->index()->unique()->change();
+          $table->bigInteger('MLSNumber')->index()->unique()->change();
       });
       Schema::table('property_images', function (Blueprint $table) {
-        $table->char('MLSNumber',20)->index()->change();
+        $table->bigInteger('MLSNumber')->index()->change();
       });
     }
 
