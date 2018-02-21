bf27cb95c373a3f8baf8b0c3e1e25e9bebf9018b
Tier5 LLC <github@tier5.us>
2018-02-21 22:08:44 +0530

    Rets api V6.4.7 stable released to dev

--- database/migrations/2018_02_21_154730_AlterMlsNumberForEveryTable.php
+++ database/migrations/2018_02_21_154730_AlterMlsNumberForEveryTable.php
@@ -13,35 +13,35 @@ class AlterMlsNumberForEveryTable extends Migration
      */
     public function up()
     {
-      Schema::table('property_details', function (Blueprint $table) {
-          $table->bigInteger('MLSNumber')->index()->unique()->change();
-          $table->dateTime('updated_at')->index()->change();
-      });
-      Schema::table('property_additionals', function (Blueprint $table) {
-          $table->bigInteger('MLSNumber')->index()->unique()->change();
-          $table->boolean('PublicAddressYN')->index()->change();
-      });
-      Schema::table('property_external_features', function (Blueprint $table) {
-          $table->bigInteger('MLSNumber')->index()->unique()->change();
-      });
-      Schema::table('property_features', function (Blueprint $table) {
-          $table->bigInteger('MLSNumber')->index()->unique()->change();
-      });
-      Schema::table('property_financial_details', function (Blueprint $table) {
-          $table->bigInteger('MLSNumber')->index()->unique()->change();
-      });
-      Schema::table('property_interior_features', function (Blueprint $table) {
-          $table->bigInteger('MLSNumber')->index()->unique()->change();
-      });
-      Schema::table('property_lat_longs', function (Blueprint $table) {
-          $table->bigInteger('MLSNumber')->index()->unique()->change();
-      });
-      Schema::table('property_locations', function (Blueprint $table) {
-          $table->bigInteger('MLSNumber')->index()->unique()->change();
-      });
-      Schema::table('property_images', function (Blueprint $table) {
-        $table->bigInteger('MLSNumber')->index()->change();
-      });
+        Schema::table('property_details', function (Blueprint $table) {
+            $table->bigInteger('MLSNumber')->index()->change();
+            $table->dateTime('updated_at')->index()->change();
+        });
+        Schema::table('property_additionals', function (Blueprint $table) {
+            $table->bigInteger('MLSNumber')->index()->unique()->change();
+            $table->boolean('PublicAddressYN')->index()->change();
+        });
+        Schema::table('property_external_features', function (Blueprint $table) {
+            $table->bigInteger('MLSNumber')->index()->unique()->change();
+        });
+        Schema::table('property_features', function (Blueprint $table) {
+            $table->bigInteger('MLSNumber')->index()->unique()->change();
+        });
+        Schema::table('property_financial_details', function (Blueprint $table) {
+            $table->bigInteger('MLSNumber')->index()->unique()->change();
+        });
+        Schema::table('property_interior_features', function (Blueprint $table) {
+            $table->bigInteger('MLSNumber')->index()->unique()->change();
+        });
+        Schema::table('property_lat_longs', function (Blueprint $table) {
+            $table->bigInteger('MLSNumber')->index()->unique()->change();
+        });
+        Schema::table('property_locations', function (Blueprint $table) {
+            $table->bigInteger('MLSNumber')->index()->unique()->change();
+        });
+        Schema::table('property_images', function (Blueprint $table) {
+          $table->bigInteger('MLSNumber')->index()->change();
+        });
     }
 
     /**
