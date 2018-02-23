708ddb6470382b1daffeed299bac9284e9c4d4d1
Tier5 LLC <github@tier5.us>
2018-02-23 18:00:17 +0530

    Rets api V7.0.0 stable released to dev

---
+++ app/PropertyMiscellaneous.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PropertyMiscellaneous extends Model
+{
+    //
+}
---
+++ database/migrations/2018_02_23_120503_create_property_miscellaneouses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePropertyMiscellaneousesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('property_miscellaneous', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('Matrix_Unique_ID')->index()->unique();
+            $table->char('MLSNumber',20)->index()->unique();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('property_miscellaneous');
+    }
+}
